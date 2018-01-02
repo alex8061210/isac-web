4a52d8c67e6f48ec68a81ff3fe24d5e6b06c4fee
alex8061210 <alex806121@gmail.com>
2018-01-02 16:43:20 +0800

    isac-web init

---
+++ src/main/java/com/chtsecurity/isac/ApplicationWeb.java
@@ -0,0 +1,15 @@
+package com.chtsecurity.isac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationWeb {
+	public static void main(String[] arg) {
+		//main alt + /
+		SpringApplication.run(ApplicationWeb.class, arg);
+		
+	}
+	
+
+}
